4db8664a70ae02538a9c72d1d1a2faf62caba532
Lucas Garron <code@garron.net>
2022-09-10 22:29:51 -0700

    FREEZE.indexed

--- adapter.ts
+++ adapter.ts
@@ -2,6 +2,8 @@ import { readFile } from 'fs/promises';
 import {
   Schema,
   initialize,
+  Adapter,
+  JsEdgeParameters,
 } from './trustfall_wasm/trustfall_wasm.js';
 
 initialize(); // Trustfall query system init.
@@ -9,3 +11,18 @@ initialize(); // Trustfall query system init.
 const schemaSource = await readFile("./schema.graphql", "utf-8")
 const SCHEMA = Schema.parse(schemaSource);
 console.log(SCHEMA)
+
+const query = `
+query {
+  Lockfile {
+    name @output
+    version @output
+  }
+}`
+
+export class LockfileAdapter implements Adapter<any> {
+  getStartingTokens(edge: string, parameters: JsEdgeParameters): IterableIterator<any> {
+      throw new Error("Sf");
+  }
+}
+
