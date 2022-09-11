6e543f8aa281f6c1975877eb64367cd2700593e7
Lucas Garron <code@garron.net>
2022-09-10 22:38:57 -0700

    FREEZE.unindexed

--- adapter.ts
+++ adapter.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs';
 import { readFile } from 'fs/promises';
 import {
   Schema,
@@ -8,21 +9,38 @@ import {
 
 initialize(); // Trustfall query system init.
 
+const PACKAGE_FOLDER = new URL("./data/", import.meta.url);
+
 const schemaSource = await readFile("./schema.graphql", "utf-8")
 const SCHEMA = Schema.parse(schemaSource);
 console.log(SCHEMA)
 
 const query = `
 query {
-  Lockfile {
+  PackageLockFile {
     name @output
     version @output
   }
 }`
 
 export class LockfileAdapter implements Adapter<any> {
-  getStartingTokens(edge: string, parameters: JsEdgeParameters): IterableIterator<any> {
-      throw new Error("Sf");
+  packageJSON: any;
+  packageLockJSON: any
+  constructor(folderPath: string | URL) {
+    this.packageJSON = JSON.parse(readFileSync(new URL("package.json", folderPath), "utf-8"));
+    this.packageLockJSON = JSON.parse(readFileSync(new URL("package-lock.json", folderPath), "utf-8"));
+  }
+
+  *getStartingTokens(edge: string, parameters: JsEdgeParameters): IterableIterator<any> {
+    switch (edge) {
+      case "PackageFile": {
+        yield this.packageJSON
+      }
+      case "PackageLockFile": {
+        yield this.packageLockJSON
+      }
+    }
   }
 }
 
+new LockfileAdapter(PACKAGE_FOLDER)
